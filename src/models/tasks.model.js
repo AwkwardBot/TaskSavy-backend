12b798348fd1f368008d166f1d63dea6b794eea7
Hassan Nadeem <itsalam90@gmail.com>
2023-09-08 11:56:43 +0500

    commented helmet

--- src/models/tasks.model.js
+++ src/models/tasks.model.js
@@ -4,3 +4,29 @@ const { toJSON, paginate } = require('./plugins');
 const { defaultBoards } = require('../config/project');
 
 
+const taskSchema = new mongoose.Schema({
+
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    assignee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User', 
+    },
+    status: {
+      type: String,
+      enum: ['To Do', 'In Progress', 'Done'], 
+      default: 'To Do',
+    },
+    sprint: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Sprint',
+    },
+    
+  });
+  
+  const Task = mongoose.model('Task', taskSchema);
+  
+  module.exports = Task
